05b87be1f011bce11b3fc10a2bcea39c8fc8cea1
Jackson-Naufal <80114615+JacksonNaufal@users.noreply.github.com>
2022-09-17 19:24:41 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,9 @@ const userInput = prompt('Enter your choice, Rock, Paper or Scissors: ')
 // checks if input is valid
 if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
   // bot gets random number from 1 - 3
-  const randomNumber = Math.floor(Math.random() * 3) + 1
+  const MAX = 3
+  const MIN = 1
+  const randomNumber = Math.floor(Math.random() * MAX) + MIN
   let botPlayer = String(randomNumber)
   // program converts number to rock, paper, scissors
   if (randomNumber === 1) {
@@ -36,11 +38,7 @@ if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
     (botPlayer === 'rock' && userInput === 'scissors')
   ) {
     console.log(`botPlayer wins! ${botPlayer} beats ${userInput}!`)
-  } else if (
-    (botPlayer === 'paper' && userInput === 'scissors') ||
-    (botPlayer === 'scissors' && userInput === 'rock') ||
-    (botPlayer === 'rock' && userInput === 'paper')
-  ) {
+  } else {
     console.log(`User wins! ${userInput} beats ${botPlayer}!`)
   }
 } else {

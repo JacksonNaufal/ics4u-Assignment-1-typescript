b80d582a10a99bce9f604365ecf9aa23330a3ef2
Jackson Naufal <Jackson.Naufal@mths.ca>
2022-09-17 19:47:24 +0000

    RockPaperScissors

--- src/index.ts
+++ src/index.ts
@@ -8,36 +8,33 @@
 
 import promptSync from 'prompt-sync'
 
-const rock = 1
-const paper = 2
-const scissors = 3
 const prompt = promptSync()
 // let ageNumber: number
 
 const userInput = prompt('Enter your choice, Rock, Paper or Scissors: ')
-if (userInput == 'rock' || userInput == 'paper' || userInput == 'scissors') {
-  let randomNumber = Math.floor(Math.random() * 3) + 1
+if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+  const randomNumber = Math.floor(Math.random() * 3) + 1
   let botPlayer = new String(randomNumber)
-  if (randomNumber == 1) {
+  if (randomNumber === 1) {
     botPlayer = 'rock'
-  } else if (randomNumber == 2) {
+  } else if (randomNumber === 2) {
     botPlayer = 'paper'
   } else {
     botPlayer = 'scissors'
   }
 
-  if (botPlayer == userInput) {
+  if (botPlayer === userInput) {
     console.log('It was a tie!')
   } else if (
-    (botPlayer == 'paper' && userInput == 'rock') ||
-    (botPlayer == 'scissors' && userInput == 'paper') ||
-    (botPlayer == 'rock' && userInput == 'scissors')
+    (botPlayer === 'paper' && userInput === 'rock') ||
+    (botPlayer === 'scissors' && userInput === 'paper') ||
+    (botPlayer === 'rock' && userInput === 'scissors')
   ) {
     console.log('botPlayer wins!')
   } else if (
-    (botPlayer == 'paper' && userInput == 'scissors') ||
-    (botPlayer == 'scissors' && userInput == 'rock') ||
-    (botPlayer == 'rock' && userInput == 'paper')
+    (botPlayer === 'paper' && userInput === 'scissors') ||
+    (botPlayer === 'scissors' && userInput === 'rock') ||
+    (botPlayer === 'rock' && userInput === 'paper')
   ) {
     console.log('User wins!')
   }

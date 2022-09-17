745abdaabd58a05c74a02fe52583a5bce8cf8144
Jackson Naufal <Jackson.Naufal@mths.ca>
2022-09-17 20:14:35 +0000

    RockPaperScissors

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const prompt = promptSync()
 const userInput = prompt('Enter your choice, Rock, Paper or Scissors: ')
 if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
   const randomNumber = Math.floor(Math.random() * 3) + 1
-  let botPlayer = new String(randomNumber)
+  let botPlayer = String(randomNumber)
   if (randomNumber === 1) {
     botPlayer = 'rock'
   } else if (randomNumber === 2) {
@@ -24,21 +24,20 @@ if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
   }
 
   if (botPlayer === userInput) {
-    console.log('It was a tie!')
+    console.log(`It was a tie! ${botPlayer} equals ${userInput}!`)
   } else if (
     (botPlayer === 'paper' && userInput === 'rock') ||
     (botPlayer === 'scissors' && userInput === 'paper') ||
     (botPlayer === 'rock' && userInput === 'scissors')
   ) {
-    console.log('botPlayer wins!')
+    console.log(`botPlayer wins! ${botPlayer} beats ${userInput}!`)
   } else if (
     (botPlayer === 'paper' && userInput === 'scissors') ||
     (botPlayer === 'scissors' && userInput === 'rock') ||
     (botPlayer === 'rock' && userInput === 'paper')
   ) {
-    console.log('User wins!')
+    console.log(`User wins! ${userInput} beats ${botPlayer}!`)
   }
-  console.log(botPlayer)
 } else {
   console.log('Invalid Input')
 }
